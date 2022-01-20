6cb717612047e3d79db8f5dfa76ce52748ad9a43
Derek <hong.wang.3@stonybrook.edu>
2022-01-20 17:19:30 -0500

    initial commit

---
+++ frontend/src/data.js
@@ -0,0 +1,44 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Cloud Bag',
+            category: 'Bags',
+            image: '/images/d1.webp',
+            price: 30,
+            brand: 'Homemade',
+            rating: 4.5,
+            numberReviews: 10
+        },
+        {
+            _id: '2',
+            name: 'Blue Sweater',
+            category: 'Sweaters',
+            image: '/images/d2.jpeg',
+            price: 50,
+            brand: 'Homemade',
+            rating: 4.0,
+            numberReviews: 20
+        },
+        {
+            _id: '3',
+            name: 'Red Hat',
+            category: 'Hats',
+            image: '/images/d3.webp',
+            price: 10,
+            brand: 'Homemade',
+            rating: 4.3,
+            numberReviews: 30
+        },
+        {
+            _id: '4',
+            name: 'Flower Bag',
+            category: 'Bags',
+            image: '/images/d4.jpeg',
+            price: 30,
+            brand: 'Homemade',
+            rating: 4.3,
+            numberReviews: 30
+        }
+    ]
+}
